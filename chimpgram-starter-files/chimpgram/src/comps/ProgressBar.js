4dcfc89a0f5eb891184b302d599d7e2009920fe8
Daniel Roman <dan.roman21@gmail.com>
2020-10-29 16:03:55 -0400

    Progress bar styling and useEffect removal

--- chimpgram-starter-files/chimpgram/src/comps/ProgressBar.js
+++ chimpgram-starter-files/chimpgram/src/comps/ProgressBar.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import useStorage from '../hooks/useStorage';
 
 const ProgressBar = ({ file, setFile }) => {
     const { url, progress } = useStorage(file);
 
+    useEffect(() => {
+        if (url) {
+            setFile(null);
+        }
+    }, [url, setFile])
+
     return (
-        <div className="progress-bar" style={{ width: progress + '%'}}>Progress</div>
+        <div className="progress-bar" style={{ width: progress + '%'}}></div>
     )
 
 }
