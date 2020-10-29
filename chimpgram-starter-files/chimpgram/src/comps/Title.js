1829085542793eeaa4d6c7451e0f6f0417771317
Daniel Roman <dan.roman21@gmail.com>
2020-10-29 14:31:42 -0400

    Initial commit

---
+++ chimpgram-starter-files/chimpgram/src/comps/Title.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Title = () => {
+  return (
+    <div className="title">
+      <h1>ChimpGram</h1>
+      <h2>You social ape, you.</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </div>
+  )
+}
+
+export default Title;
