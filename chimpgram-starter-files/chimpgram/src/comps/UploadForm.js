c2d9fc552baece8c0df2f219984b564451e2adeb
Daniel Roman <dan.roman21@gmail.com>
2020-10-29 15:06:54 -0400

    Image file upload error message work flow

--- chimpgram-starter-files/chimpgram/src/comps/UploadForm.js
+++ chimpgram-starter-files/chimpgram/src/comps/UploadForm.js
@@ -1,10 +1,30 @@
 import React from 'react';
 
 const UploadForm = () => {
+    const [file, setFile] = useState(null);
+    const [error, setError] = useState(null);
+
+    const types= ['image/png', 'image/jpeg'];
+
+    const changeHandler = (e) => {
+        let selected = e.target.files[0];
+        
+        if (selected && types.includes(selected.type)) {
+            setFile(selected);
+            setError('');
+        } else {
+            setFile(null);
+            setError('Please Select an Image File Type (png or jpeg)');
+        }
+    }
 
     return (
         <form>
-            <input type="file" />
+            <input type="file" onChange={changeHandler}/>
+            <div className="output">
+                { error && <div className="error">{ error }</div>}
+                { file && <div>{ file.name }</div> }
+            </div>
         </form>
     )
 }
