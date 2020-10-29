3c536fee46e90965c788f0c980da318383615582
Daniel Roman <dan.roman21@gmail.com>
2020-10-29 15:59:03 -0400

    Begin styling on progress bar

--- chimpgram-starter-files/chimpgram/src/comps/UploadForm.js
+++ chimpgram-starter-files/chimpgram/src/comps/UploadForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-// import ProgressBar from './ProgressBar';
+import ProgressBar from './ProgressBar';
 
 const UploadForm = () => {
   const [file, setFile] = useState(null);
@@ -7,7 +7,7 @@ const UploadForm = () => {
 
   const types = ['image/png', 'image/jpeg'];
 
-  const handleChange = (e) => {
+  const changeHandler = (e) => {
     let selected = e.target.files[0];
 
     if (selected && types.includes(selected.type)) {
@@ -18,20 +18,21 @@ const UploadForm = () => {
       setError('Please Select an Image File Type: (png or jpg)');
     }
   };
-
-    return (
+  
+  return (
     <form>
-      <label>
-        <input type="file" onChange={handleChange} />
-        <span>+</span>
+        <label>
+            <input type="file" onChange={changeHandler} />
+            <span>+</span>
       </label>
       <div className="output">
         { error && <div className="error">{ error }</div>}
         { file && <div>{ file.name }</div> }
-        {/* { file && <ProgressBar file={file} setFile={setFile} /> } */}
+        { file && <ProgressBar file={file} setFile={setFile} /> }
       </div>
     </form>
-  );
+  )
+
 }
 
 export default UploadForm;
