fff0ed09d87982555511f0d267e6bbcd34d84a58
Daniel Roman <dan.roman21@gmail.com>
2020-10-29 14:40:21 -0400

    Font family update

--- chimpgram-starter-files/chimpgram/src/comps/Title.js
+++ chimpgram-starter-files/chimpgram/src/comps/Title.js
@@ -5,7 +5,7 @@ const Title = () => {
     <div className="title">
       <h1>ChimpGram</h1>
       <h2>You social ape, you.</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <p>All of your pictures in one place. Upload now.</p>
     </div>
   )
 }
