a0e1a1fadd229a47ddd388b7ad55dc64cc3dc228
Daniel Roman <dan.roman21@gmail.com>
2020-10-29 19:55:24 -0400

    Further styling on image grid

--- chimpgram-starter-files/chimpgram/src/comps/ImageGrid.js
+++ chimpgram-starter-files/chimpgram/src/comps/ImageGrid.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import useFirestore from '../hooks/useFirestore';
+import { motion } from 'framer-motion';
 
 const ImageGrid = ({ setSelectedImg }) => {
-    const { docs } = useFirestore('images');
+  const { docs } = useFirestore('images');
 
-    return (
-        <div className="img-grid">
-            { docs && docs.map(doc => (
-                <div className="img-wrap" key={doc.id}
-                    onClick={() => setSelectedImg(doc.url)}
-                >
-                    <img src={doc.url} alt="Uploaded Picture" />
-                </div>
-            ))}
-        </div>
-    )
+  return (
+    <div className="img-grid">
+      {docs && docs.map(doc => (
+        <motion.div className="img-wrap" key={doc.id} 
+          layout
+          whileHover={{ opacity: 1 }}s
+          onClick={() => setSelectedImg(doc.url)}
+        >
+          <motion.img src={doc.url} alt="Uploaded Picture"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 1 }}
+          />
+        </motion.div>
+      ))}
+    </div>
+  )
 }
 
 export default ImageGrid;
